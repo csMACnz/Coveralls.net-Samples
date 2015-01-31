575aa0adc7765dd5acf74c503e18ab68dec5cc96
Mark Clearwater <csmacnz@csmac.co.nz>
2015-01-31 17:08:34 +1300

    Added xunit and nunit tests to the solution

---
+++ src/GameOfLife.NUnit.Tests/WorldTests.cs
@@ -0,0 +1,30 @@
+﻿using NUnit.Framework;
+
+namespace GameOfLife.NUnit.Tests
+{
+    [TestFixture]
+    public class WorldTests
+    {
+        [Test]
+        public void World_ConstructNew_WidthCorrect()
+        {
+            const int expectedWidth = 5;
+            var data = new bool[expectedWidth, 12];
+
+            var world = new World(data);
+
+            Assert.AreEqual(expectedWidth, world.Width);
+        }
+
+        [Test]
+        public void World_ConstructNew_HeightCorrect()
+        {
+            const int expectedHeight = 5;
+            var data = new bool[12, expectedHeight];
+
+            var world = new World(data);
+
+            Assert.AreEqual(expectedHeight, world.Height);
+        }
+    }
+}
---
+++ src/GameOfLife.xUnit.Tests/WorldTests.cs
@@ -0,0 +1,29 @@
+﻿using Xunit;
+
+namespace GameOfLife.xUnit.Tests
+{
+    public class WorldTests
+    {
+        [Fact]
+        public void World_ConstructNew_WidthCorrect()
+        {
+            const int expectedWidth = 5;
+            var data = new bool[expectedWidth, 12];
+
+            var world = new World(data);
+
+            Assert.Equal(expectedWidth, world.Width);
+        }
+
+        [Fact]
+        public void World_ConstructNew_HeightCorrect()
+        {
+            const int expectedHeight = 5;
+            var data = new bool[12, expectedHeight];
+
+            var world = new World(data);
+
+            Assert.Equal(expectedHeight, world.Height);
+        }
+    }
+}
